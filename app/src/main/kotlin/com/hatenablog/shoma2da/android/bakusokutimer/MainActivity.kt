daf6fb496ecbfcea39122caef666d08d91cf23a2
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-06-28 20:43:17 +0900

    Kotlinに書き換え

---
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/bakusokutimer/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hatenablog.shoma2da.android.bakusokutimer
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by shoma2da on 2014/06/28.
+ */
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstance : Bundle?) {
+        super.onCreate(savedInstance)
+        setContentView(R.layout.activity_main)
+    }
+
+}
