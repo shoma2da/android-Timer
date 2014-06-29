1ab145e7daaba92129f5d1228bfd31d327186073
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-06-30 08:25:22 +0900

    時間をタップしたら別Activityに遷移するように機能追加

---
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/bakusokutimer/CountdownActivity.kt
@@ -0,0 +1,13 @@
+package com.hatenablog.shoma2da.android.bakusokutimer
+
+import android.app.Activity
+
+/**
+ * Created by shoma2da on 2014/06/30.
+ */
+
+public class CountdownActivity : Activity() {
+
+
+
+}
--- app/src/main/kotlin/com/hatenablog/shoma2da/android/bakusokutimer/TimelistFragment.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/bakusokutimer/TimelistFragment.kt
@@ -8,6 +8,9 @@ import android.app.Fragment
 import android.widget.ListView
 import android.widget.ArrayAdapter
 import android.app.Activity
+import android.widget.TextView
+import android.util.Log
+import android.content.Intent
 
 /**
  * Created by shoma2da on 2014/06/29.
@@ -23,15 +26,21 @@ public class TimelistFragment : Fragment() {
             return view
         }
 
-        val adapter = ArrayAdapter<String>(context, R.layout.column_timelist)
-
         //とりあえず固定でデータ挿入
+        val adapter = ArrayAdapter<String>(context, R.layout.column_timelist)
         for (integer in 1..90) {
             adapter.add("${java.lang.String.format("%02d", integer)}:00")
         }
 
+        //リストの初期設定
         val list = view.findViewById(R.id.list) as ListView
         list.setAdapter(adapter)
+        list.setOnItemClickListener({parent, view, position, id ->
+            val clazz:Class<CountdownActivity> = javaClass<CountdownActivity>()
+
+            val intent = Intent(context, clazz)
+            view.getContext()?.startActivity(intent)
+        })
 
         return view
     }
