e98e021087a8369f66318df07a2cca6f37c8c873
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-24 00:06:30 +0900

    設定画面への遷移を実装

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/MainActivity.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/MainActivity.kt
@@ -3,6 +3,8 @@ package com.hatenablog.shoma2da.android.timer
 import android.app.Activity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.content.Intent
 
 /**
  * Created by shoma2da on 2014/06/28.
@@ -19,6 +21,14 @@ class MainActivity : Activity() {
         val inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item:MenuItem?):Boolean {
+        if (R.id.setting_menu == item?.getItemId()) {
+            startActivity(Intent(this, javaClass<SettingActivity>()))
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
---
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/SettingActivity.kt
@@ -0,0 +1,10 @@
+package com.hatenablog.shoma2da.android.timer
+
+import android.app.Activity
+
+/**
+ * Created by shoma2da on 2014/07/24.
+ */
+class SettingActivity : Activity() {
+
+}
