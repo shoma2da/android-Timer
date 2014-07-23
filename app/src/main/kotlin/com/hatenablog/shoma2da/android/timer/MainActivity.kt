23cb9c675149b1d80b703b5f85d41ec7328e50b4
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-24 00:01:12 +0900

    設定アイコンを配置

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/MainActivity.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hatenablog.shoma2da.android.timer
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
 
 /**
  * Created by shoma2da on 2014/06/28.
@@ -12,6 +13,12 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstance : Bundle?) {
         super.onCreate(savedInstance)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu:Menu?):Boolean {
+        val inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
