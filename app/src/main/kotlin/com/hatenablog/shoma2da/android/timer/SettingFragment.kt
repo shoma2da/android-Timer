2901b217de520246e9525dbedbb03a7f1497ad08
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-24 22:25:40 +0900

    設定画面内にバージョン情報を表示

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/SettingFragment.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/SettingFragment.kt
@@ -2,7 +2,6 @@ package com.hatenablog.shoma2da.android.timer
 
 import android.preference.PreferenceFragment
 import android.os.Bundle
-import android.util.Log
 
 /**
  * Created by shoma2da on 2014/07/24.
@@ -12,6 +11,12 @@ class SettingFragment : PreferenceFragment() {
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.preference)
+
+        //バージョン情報を設定
+        val activity = getActivity()
+        val packageManager = activity?.getPackageManager()
+        val versionName = packageManager?.getPackageInfo(activity?.getPackageName(), 0)?.versionName;
+        findPreference("version")?.setSummary(versionName)
     }
 
 }
