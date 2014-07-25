b520a7137ab82318230b0823f6acdc5f8117213d
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-26 00:04:33 +0900

    設定画面に通知ランチャーの設定を追加

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/SettingFragment.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/SettingFragment.kt
@@ -2,6 +2,8 @@ package com.hatenablog.shoma2da.android.timer
 
 import android.preference.PreferenceFragment
 import android.os.Bundle
+import android.preference.CheckBoxPreference
+import android.content.Intent
 
 /**
  * Created by shoma2da on 2014/07/24.
@@ -12,11 +14,30 @@ class SettingFragment : PreferenceFragment() {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.preference)
 
-        //バージョン情報を設定
         val activity = getActivity()
+
+        //バージョン情報を設定
         val packageManager = activity?.getPackageManager()
         val versionName = packageManager?.getPackageInfo(activity?.getPackageName(), 0)?.versionName;
         findPreference("version")?.setSummary(versionName)
+
+        //通知バーランチャーの動作設定
+        findPreference("notification_launcher")?.setOnPreferenceChangeListener { (preference, any) ->
+            when(any) {
+                true -> {
+                    if (activity != null) {
+                        activity.startService(Intent(activity, javaClass<NotificationLauncherService>()))
+                    }
+                }
+                false -> {
+                    if (activity != null) {
+                        activity.stopService(Intent(activity, javaClass<NotificationLauncherService>()))
+                    }
+                }
+                else -> {} //nothing
+            }
+            true
+        }
     }
 
 }
