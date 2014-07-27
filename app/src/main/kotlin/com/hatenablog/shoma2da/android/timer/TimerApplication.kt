4789445a7ecd247de6b00011e4bced6985524c96
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-27 13:07:11 +0900

    サンプルコードを削除

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/TimerApplication.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/TimerApplication.kt
@@ -2,8 +2,6 @@ package com.hatenablog.shoma2da.android.timer
 
 import android.app.Application
 import com.parse.Parse
-import android.util.Log
-import com.parse.ParseObject
 
 class TimerApplication : Application() {
 
@@ -11,15 +9,6 @@ class TimerApplication : Application() {
         super.onCreate()
 
         Parse.initialize(this, "JD6OCOsnYojTL0yQLqoYYBINmf7s9ugK6uHZgfBa", "GZvBpMmBaMG7ejDatcTUcBCsq9kxDOir4LYZAIIX")
-
-        //Parseテスト
-        val testObject = ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-
-        Log.d("shoma2da", "*****************************************")
-        Log.d("shoma2da", "parse saved!!")
-        Log.d("shoma2da", "*****************************************")
     }
 
 }
