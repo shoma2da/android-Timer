f31f4adafd1d259dd0166a1a086a272cea5961d1
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-27 12:03:22 +0900

    要望画面への遷移を実装

---
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/RequestActivity.kt
@@ -0,0 +1,16 @@
+package com.hatenablog.shoma2da.android.timer
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by shoma2da on 2014/07/27.
+ */
+class RequestActivity : Activity() {
+
+    override fun onCreate(bundle:Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.activity_request)
+    }
+
+}
