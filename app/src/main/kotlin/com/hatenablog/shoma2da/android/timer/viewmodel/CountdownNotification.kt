7b134550c5bc1911d5b44168c207b4569ab18355
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-08-07 22:16:21 +0900

    Notification.Buildが一定レベル以上のAPIだったためNotificationCompat.Builderに変更

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/viewmodel/CountdownNotification.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/viewmodel/CountdownNotification.kt
@@ -1,6 +1,5 @@
 package com.hatenablog.shoma2da.android.timer.viewmodel
 
-import android.app.Notification
 import android.graphics.BitmapFactory
 import com.hatenablog.shoma2da.android.timer.R
 import com.hatenablog.shoma2da.android.timer.model.RemainTime
@@ -9,6 +8,7 @@ import android.content.Intent
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
+import android.support.v4.app.NotificationCompat
 import android.app.NotificationManager
 
 /**
@@ -21,20 +21,20 @@ class CountdownNotification(val service:Service) {
         private val NOTIFICATION_ID = 1
     }
 
-    private var mNotificationBuilder:Notification.Builder? = null
+    private var mNotificationBuilder:NotificationCompat.Builder? = null
 
     fun notify(time: RemainTime) {
         val intent = Intent(service, javaClass<CountdownActivity>())
         intent.putExtra(CountdownActivity.START_COUNTDOWN_PARAM_NAME, false)
         intent.putExtra(CountdownActivity.TIME_PARAM_NAME, time)
         val pendingIntent = PendingIntent.getActivity(service, 0, intent, 0)
-        mNotificationBuilder = Notification.Builder(service).
-                setLargeIcon(BitmapFactory.decodeResource(service.getResources(), R.drawable.ic_launcher)).
-                setSmallIcon(R.drawable.ic_launcher).
-                setTicker(service.getString(R.string.notification_set_timer)).
-                setContentTitle(service.getResources()?.getString(R.string.app_name)).
-                setContentText(time.toString()).
-                setWhen(System.currentTimeMillis()).
+        mNotificationBuilder = NotificationCompat.Builder(service).
+                setLargeIcon(BitmapFactory.decodeResource(service.getResources(), R.drawable.ic_launcher))?.
+                setSmallIcon(R.drawable.ic_launcher)?.
+                setTicker(service.getString(R.string.notification_set_timer))?.
+                setContentTitle(service.getResources()?.getString(R.string.app_name))?.
+                setContentText(time.toString())?.
+                setWhen(System.currentTimeMillis())?.
                 setContentIntent(pendingIntent)
         service.startForeground(NOTIFICATION_ID, mNotificationBuilder?.build())
     }
@@ -42,7 +42,10 @@ class CountdownNotification(val service:Service) {
     fun update(time: RemainTime) {
         mNotificationBuilder?.setContentText(time.toString())
         val manager = service.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.notify(NOTIFICATION_ID, mNotificationBuilder!!.build())
+        val notification = mNotificationBuilder?.build()
+        if (notification != null) {
+            manager.notify(NOTIFICATION_ID, notification)
+        }
     }
 
     fun cancel() {
