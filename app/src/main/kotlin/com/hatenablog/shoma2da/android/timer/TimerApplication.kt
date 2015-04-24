0b7a31f03729791d89fe81582c555861d6e1ea07
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-04-24 22:44:16 +0900

    https://www.parse.com/apps/quickstart#parse_push/android/existing に沿って必要事項を記載

--- app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/TimerApplication.kt
+++ app/src/main/kotlin/com/hatenablog/shoma2da/android/timer/TimerApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.parse.Parse
 import com.google.android.gms.analytics.Tracker
 import com.google.android.gms.analytics.GoogleAnalytics
+import com.parse.ParseInstallation
 
 class TimerApplication : Application() {
 
@@ -13,6 +14,7 @@ class TimerApplication : Application() {
         super.onCreate()
 
         Parse.initialize(this, "JD6OCOsnYojTL0yQLqoYYBINmf7s9ugK6uHZgfBa", "GZvBpMmBaMG7ejDatcTUcBCsq9kxDOir4LYZAIIX")
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
     public fun getTracker() : Tracker? {
